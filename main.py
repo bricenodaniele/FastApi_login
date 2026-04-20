88214db303e2f4cb559accd85f0c24dedb32a2c7
bricenodaniele <briceno.daniele@iisgalvanimi.edu.it>
2026-04-20 07:25:51 +0000

    aggiunto main.py

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/") 
 def home():
     return FileResponse('static/home.html')
+
+@app.get("/login")
+def controlla(username : str, password : str):
+    if username == "admin" && password == "xxx123##":
+        risposta = {"messaggio": 1}
+    else:
+        risposta = {"messaggio": 0}
+    return(risposta)
