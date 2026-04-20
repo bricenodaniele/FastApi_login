5589e843e77019de5b364066527aef13a3d2c097
bricenodaniele <briceno.daniele@iisgalvanimi.edu.it>
2026-04-20 07:03:37 +0000

    aggiunto main.py non ancora fintio

--- static/script.js
+++ static/script.js
@@ -1,18 +1,11 @@
-// NUOVA FUNZIONE: Saluto
-async function ControllaCredenziali() {
-    // 1. Prendiamo quello che l'utente ha scritto
+async function controllaCredenziali() {
     const username= document.getElementById('username').value;
- const password= document.getElementById('password').value;
-    if (!username || !password ) return alert("Scrivi username e password");
-        return;
-    }
-
-    // 2. Chiamiamo il server passando il nome nell'URL (?nome=...)
-    const res = await fetch(`/login?username=${username}& password = ${password}`);
-    const dati = await res.json();
-
-    // 3. Mostriamo la risposta del server nella pagina
+    const password= document.getElementById('password').value;
+    if (!username || !password ) 
+        return alert("Scrivi username e  una password");
+    const res = await fetch("/login?username=${username}& password = ${password}");
+     const dati = await res.json(); 
     document.getElementById('risultato').innerText = dati.messaggio;
 }
 
-document.getElementById('btn-registrati').addEventListener('click', ControllaCredenziali);
+document.getElementById('btn-registrati').addEventListener('click', controllaCredenziali);
